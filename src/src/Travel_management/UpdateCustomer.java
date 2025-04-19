812fbbfd4517a28c58b0fa078d2b3256decc6bbb
OSHIL-GARG <oshilgarg361@gmail.com>
2025-04-19 16:02:52 +0530

    new commit

---
+++ src/src/Travel_management/UpdateCustomer.java
@@ -0,0 +1,203 @@
+package Travel_management;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+
+public class UpdateCustomer extends JFrame implements ActionListener {
+    JLabel labelusername,labelname;
+    JComboBox comboid;
+    JTextField tfid,tfgender,tfnumber, tfaddress, tfcountry,tfemail,tfphone;
+    JRadioButton rmale, rfemale,rother;
+    JButton add,back;
+    String username;
+    UpdateCustomer(String username){
+        this.username=username;
+        setBounds(500,200,850,550);
+        setLayout(null);
+        getContentPane().setBackground(Color.WHITE);
+
+
+        JLabel text=new JLabel("UPDATE CUSTOMER DETAILS");
+        text.setBounds(50,0,300,25);
+        text.setFont(new Font("Tahoma", Font.PLAIN,20));
+        add(text);
+
+        JLabel lblusername = new JLabel("UserName");
+        lblusername.setBounds(30, 50, 150, 25);
+        lblusername.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblusername);
+        labelusername =new JLabel();
+        labelusername.setBounds(220,50,150,25);
+        add(labelusername);
+
+
+        JLabel lblid = new JLabel("ID");
+        lblid.setBounds(30, 90, 150, 25);
+        lblid.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblid);
+        tfid=new JTextField();
+        tfid.setBounds(220,90,150,25);
+        add(tfid);
+
+
+        JLabel lblnumber = new JLabel("ID Number");
+        lblnumber.setBounds(30, 130, 150, 25);
+        lblnumber.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblnumber);
+        tfnumber=new JTextField();
+        tfnumber.setBounds(220,130,150,25);
+        add(tfnumber);
+
+        JLabel lblname = new JLabel("Name");
+        lblname.setBounds(30, 170, 150, 25);
+        lblname.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblname);
+        labelname =new JLabel();
+        labelname.setBounds(220,170,150,25);
+        add(labelname);
+
+        JLabel lblgender = new JLabel("Gender");
+        lblgender.setBounds(30, 210, 150, 25);
+        lblgender.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblgender);
+
+        tfgender=new JTextField();
+        tfgender.setBounds(220,210,150,25);
+        add(tfgender);
+
+
+
+
+        JLabel lblcountry = new JLabel("Country");
+        lblcountry.setBounds(30, 250, 150, 25);
+        lblcountry.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblcountry);
+        tfcountry=new JTextField();
+        tfcountry.setBounds(220,250,150,25);
+        add(tfcountry);
+
+        JLabel lbladdress = new JLabel("Address");
+        lbladdress.setBounds(30, 290, 150, 25);
+        lbladdress.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lbladdress);
+        tfaddress =new JTextField();
+        tfaddress.setBounds(220,290,150,25);
+        add(tfaddress);
+
+        JLabel lblemail = new JLabel("EMail");
+        lblemail.setBounds(30, 330, 150, 25);
+        lblemail.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblemail);
+        tfemail=new JTextField();
+        tfemail.setBounds(220,330,150,25);
+        add(tfemail);
+
+        JLabel lblphone = new JLabel("Phone");
+        lblphone.setBounds(30, 370, 150, 25);
+        lblphone.setFont(new Font("SAN_SERIF", Font.PLAIN, 20));
+        add(lblphone);
+        tfphone=new JTextField();
+        tfphone.setBounds(220,370,150,25);
+        add(tfphone);
+
+        add=new JButton("Update");
+        add.setBackground(Color.black);
+        add.setForeground(Color.white);
+        add.setBounds(70,430,100,25);
+        add.addActionListener(this);
+        add(add);
+
+        back=new JButton("BACK");
+        back.setBackground(Color.black);
+        back.setForeground(Color.white);
+        back.setBounds(70,470,100,25);
+        back.addActionListener(this);
+        add(back);
+
+        try {
+            // Load image correctly
+            ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/icons/UPdate.png"));
+            Image i2 = i1.getImage().getScaledInstance(300, 400, Image.SCALE_DEFAULT);
+            ImageIcon i3 = new ImageIcon(i2);
+            // Check if the image is null (not found)
+            if (i1.getImage() == null) {
+                throw new NullPointerException("Error: Image not found! Check the path.");
+            }
+
+            JLabel image = new JLabel(i3);
+            image.setBounds(500, 80, 300, 400);
+
+            add(image);
+
+        } catch (NullPointerException e) {
+            System.out.println(e.getMessage()); // Print error message to console
+            JOptionPane.showMessageDialog(this, "Image not found! Check the file path.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+
+
+
+
+        try {
+            Conn c=new Conn();
+            ResultSet rs=c.s.executeQuery("select * from customer where username='"+username+"'");
+            while (rs.next()){
+                labelusername.setText(rs.getString("username"));
+                labelname.setText(rs.getString("name"));
+                tfid.setText(rs.getString("id"));
+                tfnumber.setText(rs.getString("number"));
+                tfphone.setText(rs.getString("phone"));
+                tfcountry.setText(rs.getString("country"));
+                tfemail.setText(rs.getString("email"));
+                tfgender.setText(rs.getString("gender"));
+                tfaddress.setText(rs.getString("address"));
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+
+        setVisible(true);
+
+    }
+
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource()==add){
+            String username=labelusername.getText();
+            String id=tfid.getText();
+            String number= tfnumber.getText();
+            String name=labelname.getText();
+            String gender=tfgender.getText();
+            String country=tfcountry.getText();
+            String address= tfaddress.getText();
+            String email=tfemail.getText();
+            String phone=tfphone.getText();
+
+            try {
+
+                Conn c=new Conn();
+                String query="update customer set username='"+username+"',id='"+id+"',number='"+number+"',name='"+name+"',gender='"+gender+"',country='"+country+"',address='"+address+"',email='"+email+"',phone='"+phone+"'";
+                c.s.executeUpdate(query);
+
+                JOptionPane.showMessageDialog(null,"Customer Details Updated Successfully");
+                setVisible(false);
+
+
+            }catch (Exception e){
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Error while adding customer details");
+            }
+
+        }else {
+            setVisible(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        new UpdateCustomer("qwerty");
+    }
+}
